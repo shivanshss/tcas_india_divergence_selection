c58eb195b37810ef9e0e5c8ffcad753417885e83
ss <48706782+shivanshss@users.noreply.github.com>
2023-08-24 15:05:49 +0530

    Update align_sort_index_variant_call_annotate.sh

--- step1/align_sort_index_variant_call_annotate.sh
+++ step1/align_sort_index_variant_call_annotate.sh
@@ -20,7 +20,7 @@ for sample_name in "${!sample_info[@]}"; do
     read2="${read_files_arr[1]}"
 
     # Step 1: Align reads using BWA
-    "$bwa" mem -t "$num_threads" "$reference_genome" "$read1" "$read2" > "${sample_name}_aligned.sam"
+    "$bwa" mem -B 8 -t "$num_threads" "$reference_genome" "$read1" "$read2" > "${sample_name}_aligned.sam"
 
     # Step 2: Convert SAM to BAM
     "$samtools" view -S -b "${sample_name}_aligned.sam" > "${sample_name}_aligned.bam"
@@ -33,22 +33,25 @@ for sample_name in "${!sample_info[@]}"; do
 
     # Clean up intermediate files
     rm "${sample_name}_aligned.sam" "${sample_name}_aligned.bam"
+
+
+    # Step 5: Remove PCR duplicates using PicardTools
+    java -jar "$picard_jar" MarkDuplicates I=${sample_name}_sorted.bam O=${sample_name}_deduplicated.bam M=${sample_name}_dedup_metrics.txt REMOVE_DUPLICATES=T REMOVE_SEQUENCING_DUPLICATES=T
+
+    # Step 6: Call SNPs using GATK HaplotypeCaller
+    "$gatk" HaplotypeCaller -R "$reference_genome" -I ${sample_name}_deduplicated.bam -O ${sample_name}_raw_variants.vcf -ERC GVCF --variant_index_type LINEAR --variant_index_parameter 128000
+
+    # Step 7: Convert genotyped VCF to VCF format using GenotypeGVCFs
+    "$gatk" --java-options "-Xmx128g" GenotypeGVCFs -R "$reference_genome" -V ${sample_name}_raw_variants.vcf -O ${sample_name}_merged_variants.vcf
+        
+    # Step 8: Filter merged VCF
+    "$gatk" VariantFiltration -R "$reference_genome" -V ${sample_name}_merged_variants.vcf -filter "QD < 2.0" --filterName "QD2" -filter "QUAL < 30.0" --filterName "QUAL30" -filter "SOR > 3.0" --filterName "SOR3" -filter "FS > 60.0" --filterName "FS60" -filter "MQ < 40.0" --filterName "MQ40" -filter "MQRankSum < -12.5" --filterName "MQRankSum-12.5" -filter "ReadPosRankSum < -8.0" --filterName "ReadPosRankSum-8" -O ${sample_name}_filtered_variants.vcf
+
+    # Step 9: Annotate SNPs using ANNOVAR
+    perl "$annovar" ${sample_name}_filtered_variants.vcf "$annovar_db" -out ${sample_name}_annotated -vcfinput -remove -protocol refGeneWithVer -operation g -format vcf4 -allsample -withfreq -withzyg -nastring . -csvout -polish
+
 done
 
-# Step 5: Remove PCR duplicates using PicardTools
-java -jar "$picard_jar" MarkDuplicates I=sample1_sorted.bam O=sample1_deduplicated.bam M=sample1_dedup_metrics.txt
-
-# Step 6: Call SNPs using GATK HaplotypeCaller
-"$gatk" HaplotypeCaller -R "$reference_genome" -I sample1_deduplicated.bam -O sample1_raw_variants.vcf
-
-# Step 7: Convert genotyped VCF to VCF format using GenotypeGVCFs
-"$gatk" GenotypeGVCFs -R "$reference_genome" -V sample1.vcf -O sample1_merged.vcf
-
-# Step 8: Filter merged VCF
-"$gatk" VariantFiltration -R "$reference_genome" -V sample1_merged.vcf --filter-expression "QUAL < 20 || DP < 5 || DP > 500 || AF < 0.1 || AF > 0.8 || ExcessHet > 54.69 || InbreedingCoeff < -0.3" --filter-name "custom_filter" -O sample1_filtered.vcf
-
-# Step 9: Annotate SNPs using ANNOVAR
-"$annovar" sample1_filtered.vcf "$annovar_db" -buildver "$genome_build" -out sample1_annotated -remove -protocol refGene -operation g
 
 echo "Analysis complete!"
 
